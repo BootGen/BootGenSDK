7293ddfca30704f9b042d98e68e51d9eb96d18bb
Gabor Angyal <angyalgabor@outlook.com>
2021-04-07 16:40:47 +0200

    add dummy generation test

--- BootGenTest/ProjectTest.cs
+++ BootGenTest/ProjectTest.cs
@@ -12,8 +12,20 @@ namespace BootGenTest
     [TestClass]
     public class ProjectTest
     {
+
         [TestMethod]
         public void TestGenerate()
+        {
+            TestWithTemplates("templates");
+        }
+
+        [TestMethod]
+        public void TestWithoutTemplates()
+        {
+            TestWithTemplates("does_not_exists");
+        }
+
+        private static void TestWithTemplates(string templateRoot)
         {
             var data = JObject.Parse(File.ReadAllText("example_input.json"), new JsonLoadSettings { CommentHandling = CommentHandling.Load });
             var dataModel = new DataModel();
@@ -33,28 +45,37 @@ namespace BootGenTest
                 Disk = disk,
                 ResourceCollection = resourceCollection,
                 SeedStore = seedStore,
-                TemplateRoot = "templates"
+                TemplateRoot = templateRoot
             };
             project.GenerateFiles("TestProject", "TestProject", "http://localhost:5000");
             if (Directory.Exists("SampleOutput"))
                 Directory.Delete("SampleOutput", true);
-            try {
+            try
+            {
                 ZipFile.ExtractToDirectory("SampleOutput.zip", ".");
-            } catch {
-                if (File.Exists("SampleOutput.zip")) {
+            }
+            catch
+            {
+                if (File.Exists("SampleOutput.zip"))
+                {
                     Assert.Fail($"SampleOutput.zip length: {new FileInfo("SampleOutput.zip").Length}");
-                } else {
+                }
+                else
+                {
                     Assert.Fail("SampleOutput.zip is missing!");
                 }
             }
-            foreach (var file in disk.Files) {
-               var path = System.IO.Path.Combine("SampleOutput", file.Path, file.Name);
+            foreach (var file in disk.Files)
+            {
+                var path = System.IO.Path.Combine("SampleOutput", file.Path, file.Name);
                 var expectedLines = File.ReadAllLines(path);
                 var actualLines = file.Content.Split(Environment.NewLine);
-                for (int i = 0; i < expectedLines.Length; ++i) {
+                for (int i = 0; i < expectedLines.Length; ++i)
+                {
                     Assert.AreEqual(expectedLines[i], actualLines[i], false, $"{file.Name} line {i}.");
                 }
-                if (expectedLines.Length + 1 == actualLines.Length) {
+                if (expectedLines.Length + 1 == actualLines.Length)
+                {
                     string lastLine = actualLines.Last();
                     Assert.IsTrue(string.IsNullOrWhiteSpace(lastLine), lastLine);
                     continue;
