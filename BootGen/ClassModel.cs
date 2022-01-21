0a9139f231d57203716dcf51e111c07339b9cda2
Gabor Angyal <gabor.angyal@codesharp.hu>
2022-01-21 06:59:38 +0100

    BaseProperties

--- BootGen/ClassModel.cs
+++ BootGen/ClassModel.cs
@@ -17,6 +17,7 @@ namespace BootGen
         public bool IsRoot { get; set; }
         public Property IdProperty => PropertyWithName("Id");
         public List<Property> CommonProperties => Properties.Where(p => !p.IsServerOnly).ToList();
+        public List<Property> BaseProperties => CommonProperties.Where(p => !p.IsKey).ToList();
         public List<Property> JsonProperties => Properties.Where(p => !p.IsKey && !p.IsParentReference).ToList();
         public List<Property> ChildReferences => Properties.Where(p => p.BuiltInType == BuiltInType.Object && !p.IsParentReference).ToList();
 
