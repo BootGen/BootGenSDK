f704aa81c176eeec7a47abf8df313b8965cde0ae
Gabor Angyal <gabor.angyal@codesharp.hu>
2022-02-13 06:11:28 +0100

    create DiskTemplateLoader

---
+++ BootGen/DiskTemplateLoader.cs
@@ -0,0 +1,32 @@
+using System.Threading.Tasks;
+using Scriban;
+using Scriban.Parsing;
+using Scriban.Runtime;
+
+namespace BootGen;
+
+public class DiskTemplateLoader : ITemplateLoader
+{
+    IDisk Disk { get; }
+
+    public DiskTemplateLoader(IDisk disk)
+    {
+        Disk = disk;
+    }
+
+    public string GetPath(TemplateContext context, SourceSpan callerSpan, string templateName)
+    {
+        return templateName;
+    }
+
+    public string Load(TemplateContext context, SourceSpan callerSpan, string templatePath)
+    {
+        return Disk.GetFileContent("templatePath");
+    }
+
+    public ValueTask<string> LoadAsync(TemplateContext context, SourceSpan callerSpan, string templatePath)
+    {
+        return ValueTask.FromResult(Load(context, callerSpan, templatePath));
+    }
+}
+
--- BootGen/GeneratorBase.cs
+++ BootGen/GeneratorBase.cs
@@ -5,115 +5,119 @@ using Scriban;
 using Scriban.Runtime;
 using Scriban.Syntax;
 
-namespace BootGen
+namespace BootGen;
+public class GeneratorBase : ScriptObject
 {
-    public class GeneratorBase : ScriptObject
+    public string NameSpace { get; set; }
+    public IDisk Disk { get; }
+    public IDisk Templates { get; set; }
+
+    public GeneratorBase(IDisk disk)
     {
-        public string NameSpace { get; set; }
-        public IDisk Disk { get; }
-        public IDisk Templates { get; set; }
+        Disk = disk;
+    }
 
-        public GeneratorBase(IDisk disk)
+    public static string SnakeCase(string value)
+    {
+        return value.ToSnakeCase();
+    }
+
+    public static string KebabCase(string value)
+    {
+        return value.ToKebabCase();
+    }
+
+    public static string CamelCase(string value)
+    {
+        return value.ToCamelCase();
+    }
+    public static string ToWords(string value)
+    {
+        return value.ToWords();
+    }
+
+    public void Render(string folderName, string targetFileName, string templateFile, Dictionary<string, object> parameters)
+    {
+        string content = Render(templateFile, parameters);
+        if (!string.IsNullOrEmpty(content))
+            Disk.WriteText(folderName, targetFileName, content);
+    }
+
+    public string Render(string templateFile, Dictionary<string, object> parameters)
+    {
+        var template = LoadTemplate(templateFile);
+        if (template == null)
         {
-            Disk = disk;
+            return null;
         }
+        var context = CreateContext();
+        context.PushGlobal(this);
+        foreach (var param in parameters)
+            context.SetValue(new ScriptVariableGlobal(param.Key), param.Value);
+        return template.Render(context);
+    }
 
-        public static string SnakeCase(string value)
-        {
-            return value.ToSnakeCase();
+    private TemplateContext CreateContext()
+    {
+        return new TemplateContext {
+            TemplateLoader = new DiskTemplateLoader(Templates)
+        };
+    }
+
+    Template LoadTemplate(string templateFile) {
+        var template = Parse(templateFile);
+        if (template == null) {
+            Console.WriteLine($"File not found: {templateFile}");
         }
-
-        public static string KebabCase(string value)
-        {
-            return value.ToKebabCase();
-        }
-
-        public static string CamelCase(string value)
-        {
-            return value.ToCamelCase();
-        }
-        public static string ToWords(string value)
-        {
-            return value.ToWords();
-        }
-
-        public void Render(string folderName, string targetFileName, string templateFile, Dictionary<string, object> parameters)
-        {
-            string content = Render(templateFile, parameters);
-            if (!string.IsNullOrEmpty(content))
-                Disk.WriteText(folderName, targetFileName, content);
-        }
-
-        public string Render(string templateFile, Dictionary<string, object> parameters)
-        {
-            var template = LoadTemplate(templateFile);
-            if (template == null) {
-                return null;
-            }
-            var context = new TemplateContext();
-            context.PushGlobal(this);
-            foreach (var param in parameters)
-                context.SetValue(new ScriptVariableGlobal(param.Key), param.Value);
-            return template.Render(context);
-        }
-
-        Template LoadTemplate(string templateFile) {
-            var template = Parse(templateFile);
-            if (template == null) {
-                Console.WriteLine($"File not found: {templateFile}");
-            }
-            return template;
-        }
-
-
-        public void RenderClasses(string folderName, Func<ClassModel, string> targetFileName, string templateFile, IEnumerable<ClassModel> classes)
-        {
-            var template = LoadTemplate(templateFile);
-            if (template == null) {
-                return;
-            }
-            foreach (var c in classes)
-            {
-                Disk.WriteText(folderName, targetFileName(c), RenderClass(template, c));
-            }
-        }
-
-        private string RenderClass(Template template, ClassModel c)
-        {
-            var context = new TemplateContext();
-            context.PushGlobal(this);
-            context.SetValue(new ScriptVariableGlobal("name_space"), NameSpace);
-            context.SetValue(new ScriptVariableGlobal("class"), c);
-            return template.Render(context);;
-        }
-
-        public void RenderResources(string folderName, Func<Resource, string> targetFileName, string templateFile, IEnumerable<Resource> resources)
-        {
-            var template = LoadTemplate(templateFile);
-            if (template == null) {
-                return;
-            }
-            foreach (var resource in resources)
-            {
-                var context = new TemplateContext();
-                context.PushGlobal(this);
-                context.SetValue(new ScriptVariableGlobal("name_space"), NameSpace);
-                context.SetValue(new ScriptVariableGlobal("resource"), resource);
-                var renderedController = template.Render(context);
-                Disk.WriteText(folderName, targetFileName(resource), renderedController);
-            }
-        }
-
-        private Template Parse(string templateFile)
-        {
-            string content = Templates.GetFileContent(templateFile);
-            if (string.IsNullOrWhiteSpace(content))
-                return null;
-            return Template.Parse(content, templateFile);
-        }
-
+        return template;
     }
 
 
+    public void RenderClasses(string folderName, Func<ClassModel, string> targetFileName, string templateFile, IEnumerable<ClassModel> classes)
+    {
+        var template = LoadTemplate(templateFile);
+        if (template == null) {
+            return;
+        }
+        foreach (var c in classes)
+        {
+            Disk.WriteText(folderName, targetFileName(c), RenderClass(template, c));
+        }
+    }
+
+    private string RenderClass(Template template, ClassModel c)
+    {
+        var context = CreateContext();
+        context.PushGlobal(this);
+        context.SetValue(new ScriptVariableGlobal("name_space"), NameSpace);
+        context.SetValue(new ScriptVariableGlobal("class"), c);
+        return template.Render(context);;
+    }
+
+    public void RenderResources(string folderName, Func<Resource, string> targetFileName, string templateFile, IEnumerable<Resource> resources)
+    {
+        var template = LoadTemplate(templateFile);
+        if (template == null) {
+            return;
+        }
+        foreach (var resource in resources)
+        {
+            var context = CreateContext();
+            context.PushGlobal(this);
+            context.SetValue(new ScriptVariableGlobal("name_space"), NameSpace);
+            context.SetValue(new ScriptVariableGlobal("resource"), resource);
+            var renderedController = template.Render(context);
+            Disk.WriteText(folderName, targetFileName(resource), renderedController);
+        }
+    }
+
+    private Template Parse(string templateFile)
+    {
+        string content = Templates.GetFileContent(templateFile);
+        if (string.IsNullOrWhiteSpace(content))
+            return null;
+        return Template.Parse(content, templateFile);
+    }
 
 }
+
